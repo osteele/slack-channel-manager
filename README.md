c8ebd04ce44c9806bc81146376ca256553674d98
Oliver Steele <steele@osteele.com>
2021-06-07 16:39:12 +0000

    Doc required scopes; reformat code

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 ## Install
 
+Create a Slack application with these scopes: channels:read, channels:write, groups:read, groups:write, im:read, mpim:read. Install it in the workspace. Set SLACK_OAUTH_TOKEN to the User OAuth Token.
+
+Run these in the terminal:
+
 ```
 pip install slack_sdk
 pip install pandas
--- create_channels.py
+++ create_channels.py
@@ -14,35 +14,38 @@ output_csv_path = 'channel-ids.csv'
 
 client = WebClient(token=os.environ["SLACK_OAUTH_TOKEN"])
 
+
 def list_channels():
-  channels = []
-  next_cursor = None
-  while True:
-    response = client.conversations_list(limit=1000, cursor=next_cursor)
-    channels += response['channels']
-    next_cursor = response['response_metadata']['next_cursor']
-    if not next_cursor:
-      break
-  return channels
+	channels = []
+	next_cursor = None
+	while True:
+		response = client.conversations_list(limit=1000, cursor=next_cursor)
+		channels += response['channels']
+		next_cursor = response['response_metadata']['next_cursor']
+		if not next_cursor:
+			break
+	return channels
+
 
 def find_or_create_channel(channel_name, topic=None, purpose=None):
-  action = 'Found'
-  channel = next((c for c in channels if c['name'] == channel_name), None)
-  if not channel:
-    action = 'Created'
-    try:
-      response = client.conversations_create(name=channel_name)
-      channel = response['channel']
-    except SlackApiError as e:
-      print("Error creating conversation: {}".format(e))
-      sys.exit(1)
-  if purpose and channel['purpose']['value'] != purpose:
-    print(f"Update {channel['name']} purpose to {purpose}")
-    client.conversations_setPurpose(channel=channel['id'], purpose=purpose)
-  if topic and channel['topic']['value'] != topic:
-    print(f"Update {channel['name']} topic to {topic}")
-    client.conversations_setTopic(channel=channel['id'], topic=topic)
-  return channel, action
+	action = 'Found'
+	channel = next((c for c in channels if c['name'] == channel_name), None)
+	if not channel:
+		action = 'Created'
+		try:
+			response = client.conversations_create(name=channel_name)
+			channel = response['channel']
+		except SlackApiError as e:
+			print("Error creating conversation: {}".format(e))
+			sys.exit(1)
+	if purpose and channel['purpose']['value'] != purpose:
+		print(f"Update {channel['name']} purpose to {purpose}")
+		client.conversations_setPurpose(channel=channel['id'], purpose=purpose)
+	if topic and channel['topic']['value'] != topic:
+		print(f"Update {channel['name']} topic to {topic}")
+		client.conversations_setTopic(channel=channel['id'], topic=topic)
+	return channel, action
+
 
 channels = list_channels()
 
@@ -52,18 +55,24 @@ channels = list_channels()
 
 channels_pd = pd.read_csv(input_csv_path)
 if 'Name' not in channels_pd.columns:
-  print("CSV file requires a Name column")
-  sys.exit(1)
+	print("CSV file requires a Name column")
+	sys.exit(1)
 
-channel_ids_df = pd.DataFrame([], columns = ['Name', 'ID'])
+channel_ids_df = pd.DataFrame([], columns=['Name', 'ID'])
 
 for _, row in channels_pd.iterrows():
-  channel_name = row['Name']
-  channel, action = find_or_create_channel(channel_name, purpose=row.get('Purpose'), topic=row.get('Topic'))
-  channel_ids_df = channel_ids_df.append({'Name': channel_name, 'ID': channel['id']}, ignore_index=True)
-  print(f"{action} {channel_name}")
-  
+	channel_name = row['Name']
+	channel, action = find_or_create_channel(channel_name,
+	                                         purpose=row.get('Purpose'),
+	                                         topic=row.get('Topic'))
+	channel_ids_df = channel_ids_df.append(
+	    {
+	        'Name': channel_name,
+	        'ID': channel['id']
+	    }, ignore_index=True)
+	print(f"{action} {channel_name}")
+
 with open(output_csv_path, 'w') as f:
-  f.write(channel_ids_df.to_csv(index=False))
+	f.write(channel_ids_df.to_csv(index=False))
 
 print(f"Wrote channel ids to {output_csv_path}")
