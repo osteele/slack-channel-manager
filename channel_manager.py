f3607ecc6271bef082056e8c11e27aab71dbb0f0
Oliver Steele <steele@osteele.com>
2021-06-12 10:45:32 +0000

    First pass on add_channel_members

--- channel_manager.py
+++ channel_manager.py
@@ -3,7 +3,6 @@ import sys
 import pandas as pd
 import csv
 import time
-from pathlib import Path
 
 import click
 from jinja2 import Template
@@ -162,6 +161,7 @@ def post_messages(csv_path, template_path, dry_run, limit, pin):
       if pin:
         client.pins_add(channel=channel['id'], timestamp=response['ts'])
 
+
 @click.command()
 @click.argument('to_pin', default='to_pin.csv')
 @click.argument('pin_lookup_path', default='pin_lookup.csv')
@@ -201,3 +201,102 @@ def set_pins(to_pin, pin_lookup_path, dry_run):
     c.writerow(['channel', 'ts'])
     c.writerows([*pin_lookup.items()])
   print('ok')
+
+
+@click.command()
+@click.option('--dry-run/--no-dry-run', default=False)
+@click.option('--channel_limit', type=int)
+@click.argument('member_channels_csv', type=click.File())
+def add_channel_members(member_channels_csv, channel_limit, dry_run):
+  df = load_csv(member_channels_csv, limit=channel_limit, required_headers=['Member'])
+
+  channel_names = [cn for cn in df.columns if cn != 'Member']
+  channels = list_channels()
+  channels = [c for cn in channel_names for c in channels if c['name'] == cn]
+  if len(channels) < len(channel_names):
+    print("Missing channels:", ' '.join(cn for cn in channel_names if cn not in {c['name'] for c in channels}))
+  channels_by_name = {c['name']: c for c in channels}
+
+  df['Email'] = df.Member.str.extract(r'<(.+?)(?: \(i was registered.*)?>')
+
+  users_by_email = {u['profile']['email']: u
+    for u in get_user_list()
+    if u['profile'].get('email', None)}
+  users_by_id = {u['id']: u
+    for u in users_by_email.values()}
+
+  # uxf = [
+  #   dict(
+  #     username=u['name'],
+  #     email=u['profile'].get('email', None),
+  #     userid=u['id'],
+  #     fullname=u['profile']['real_name'],
+  #     displayname=u['profile']['display_name'],
+  #    ) for u in get_user_list()]
+  # dfx = pd.DataFrame(uxf)
+  # print(uxf)
+  # dfx.sort_values(by=['Name'], inplace=True)
+  # with open('members.csv', 'w') as f:
+  #   f.write(dfx.to_csv(index=False))
+
+  channel_invitations = []
+
+  for channel in channels:
+    print(f"{channel['name']}:")
+    current_member_ids = get_conversation_members(channel['id'])
+    current_member_emails = {users_by_id[uid]['profile']['email'] for uid in current_member_ids if uid in users_by_id}
+    invited_member_emails = set(df[df[channel['name']] == 'y'].Email)
+    # invited_member_ids = [users_by_email[u] for u in invited_member_emails if u in users_by_email]
+
+    unregistered_invitees = {u for u in invited_member_emails if u not in users_by_email}
+    if unregistered_invitees:
+      print('. Participants who are not in Slack:', ' '.join(unregistered_invitees))
+    
+    # print('. Current channel members who are not in the spreadsheet', current_member_emails - invited_member_emails)
+
+    could_invite = invited_member_emails - current_member_emails - unregistered_invitees
+    if could_invite:
+      print('. Slack members who are not in the channel', ' '.join(could_invite))
+
+
+    # print(channel['name'], target_member_emails, target_member_ids)
+  #   # print(channel['name'], member_ids)
+  #   members = [users_by_id[uid] for uid in member_ids if uid in users_by_id]
+  #   # print([u['name'] for u in members])
+  #   # print([u['profile']['email'] for u in members])
+
+
+def load_csv(file_or_url, limit=None, required_headers=[]):
+  if file_or_url.name.endswith('.url'):
+    file_or_url = file_or_url.read()
+  df = pd.read_csv(file_or_url)
+  missing_headers = [h for h in required_headers if h not in df.columns]
+  if missing_headers:
+    die(f"The following required headers were not found in {file_or_url}: {' '.join(missing_headers)}")
+  if limit:
+    df.drop(df.index[limit:], inplace=True)
+  return df
+
+
+def get_conversation_members(channel_id):
+  members = []
+  cursor = None
+  while True:
+    response = client.conversations_members(channel=channel_id, cursor=cursor, limit=100)
+    members += response['members']
+    cursor = response['response_metadata']['next_cursor']
+    if not cursor:
+      break
+  return set(members)
+
+
+def get_user_list():
+  members = []
+  cursor = None
+  while True:
+    response = client.users_list(cursor=cursor, limit=100)
+    members += response['members']
+    cursor = response['response_metadata']['next_cursor']
+    if not cursor:
+      break
+  return members
